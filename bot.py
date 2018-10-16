b0504b18393ee64e1589aa4ff6a6272904c021a9
hiyarcocuk32 <43614317+hiyarcocuk32@users.noreply.github.com>
2018-10-16 21:55:36 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,68 @@
+import discord                      #"py -m pip install discord" yap başta
+from discord.ext.commands import bot
+from discord.ext import commands
+import random
+import asyncio
+import time
+ 
+##PREFIX##
+bot = commands.Bot(command_prefix='=')
+ 
+##BOT IS READY##
+@bot.event
+async def on_ready():
+    print("Bot Server Çökertmeye Hazır")
+ 
+##SPAM COMMAND##
+@bot.command(pass_context=True)
+async def sa(ctx): #"=sa" yaz çalıştır
+    while True:
+        await bot.say("/tts HackedBySmorgs:D\n/tts HackedBySmorgs:D\n/tts HackedBySmorgs:D\n/tts HackedBySmorgs:D\n")
+        await bot.say("/tts HackedBySmorgs:D\n/tts HackedBySmorgs:D\n/tts HackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        await bot.say("HackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\nHackedBySmorgs:D\n")
+        
+
+##BOT TOKEN##
+bot.run ("NTAxNDY3OTQ2NTI3MzU5MDA2.DqZ0mQ.oxfdRb5lJrPjYtE32LhmWUHzNpg")
